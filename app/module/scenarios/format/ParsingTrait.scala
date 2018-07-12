79d3f0ec13b1f6263cce2955566f515ec163c6ed
Faiz <891366402@qq.com>
2018-07-12 19:38:27 +0800

    就差医院详情

--- app/module/scenarios/format/ParsingTrait.scala
+++ app/module/scenarios/format/ParsingTrait.scala
@@ -1,5 +1,6 @@
 package module.scenarios.format
 
+import com.pharbers.ErrorCode
 import play.api.libs.json.JsValue
 import play.api.libs.json.Json.toJson
 
@@ -12,48 +13,99 @@ trait ParsingTrait {
 		}
 	}
 	
-	def filterZip(data: JsValue)(out: JsValue => String Map JsValue): String Map JsValue = out(data)
-	
-	val zfcDestGoods: JsValue => String Map JsValue = { data =>
-		val reVal = (data \ "current" \ "dest_goods").as[List[String Map JsValue]].groupBy(g => g("dest_id").as[String]).map { dg =>
-			
-			val hospital = (data \ "current" \ "connect_dest").as[List[String Map JsValue]].find(f => f("id").as[String] == dg._1).map ( x =>
-				Map("hospid" -> x("id"),
-					"name" -> x("hosp_name"),
-					"category" -> x("category"),
-					"hosp_level" -> x("hosp_level"),
-					"department" -> x("department"),
-					"beds" -> x("beds"),
-					"outpatient" -> x("outpatient_year"),
-					"surgery" -> x("surgery_year")
-				)
-			).getOrElse(throw new Exception("is null"))
-			
-			val goods = dg._2.map { g =>
-				(data \ "current" \ "connect_goods").as[List[String Map JsValue]].find(f => g("goods_id").as[String] == f("id").as[String]).map ( x =>
-					Map("id" -> x("id"),
-						"name" -> x("category"),
-						"share" -> (g("relationship") \ "share").as[JsValue],
-						"previoussales" -> (g("relationship") \ "pre_sales").as[JsValue],
-						"marketpotential" -> (g("relationship") \ "potential").as[JsValue],
-						"contributionrate" -> (g("relationship") \ "cont_rate").as[JsValue])
+	val formatHospitals: String Map JsValue => (Option[String Map JsValue], Option[JsValue])  = { m =>
+		try {
+			val data = m("scenario")
+			val reVal = (data \ "current" \ "dest_goods").as[List[String Map JsValue]].groupBy(g => g("dest_id").as[String]).map { dg =>
 				
+				val hospital = (data \ "current" \ "connect_dest").as[List[String Map JsValue]].find(f => f("id").as[String] == dg._1).map ( x =>
+					Map("hospid" -> x("id"),
+						"name" -> x("hosp_name"),
+						"category" -> x("category"),
+						"hosp_level" -> x("hosp_level"),
+						"department" -> x("department"),
+						"beds" -> x("beds"),
+						"outpatient" -> x("outpatient_year"),
+						"surgery" -> x("surgery_year")
+					)
 				).getOrElse(throw new Exception("is null"))
-			}
-			
-			val representative = (data \ "current" \ "dest_reso").as[List[String Map JsValue]].filter(f => dg._1 == f("dest_id").as[String]).flatMap ( x =>
-				(data \ "current" \ "connect_reso").as[List[String Map JsValue]].filter(ff => x("reso_id").as[String] == ff("id").as[String]).map( r =>
-					Map("name" -> r("rep_name"), "avatar" -> r("avatar"))
+				
+				val goods = dg._2.map { g =>
+					(data \ "current" \ "connect_goods").as[List[String Map JsValue]].find(f => g("goods_id").as[String] == f("id").as[String]).map ( x =>
+						Map("id" -> x("id"),
+							"name" -> x("category"),
+							"share" -> (g("relationship") \ "share").as[JsValue],
+							"previoussales" -> (g("relationship") \ "pre_sales").as[JsValue],
+							"marketpotential" -> (g("relationship") \ "potential").as[JsValue],
+							"contributionrate" -> (g("relationship") \ "cont_rate").as[JsValue])
+					
+					).getOrElse(throw new Exception("is null"))
+				}
+				
+				val representative = (data \ "current" \ "dest_reso").as[List[String Map JsValue]].filter(f => dg._1 == f("dest_id").as[String]).flatMap ( x =>
+					(data \ "current" \ "connect_reso").as[List[String Map JsValue]].filter(ff => x("reso_id").as[String] == ff("id").as[String]).map( r =>
+						Map("name" -> r("rep_name"), "avatar" -> r("avatar"))
+					)
 				)
-			)
+				
+				hospital ++ Map("medicines" -> toJson(goods)) ++ Map("representives" -> toJson(representative))
+				
+			}.toList
 			
-			hospital ++ Map("medicines" -> toJson(goods)) ++ Map("representives" -> toJson(representative))
-			
-		}.toList
-		Map("result" -> toJson(
+			(Some(Map("result" -> toJson(
 				Map("currentMonth" -> toJson((data \ "current" \ "name").as[String]),
 					"hospitals" -> toJson(reVal)
 				))
-		)
+			)), None)
+		} catch {
+			case ex: Exception => (None, Some(ErrorCode.errorToJson(ex.getMessage)))
+		}
+		
+		
+	}
+	
+	val formatBudget: String Map JsValue => (Option[String Map JsValue], Option[JsValue]) = { m =>
+		try {
+			val data = m("scenario")
+			val total = (data \ "current" \ "connect_reso").as[List[String Map JsValue]].
+				find(f => f("type").as[String] == "money").map( x =>
+				Map("total" -> (x("relationship") \ "total_budget_money").as[JsValue])
+			).getOrElse(throw new Exception("is null"))
+			val used = (data \ "current" \ "dest_goods_reso").as[List[String Map JsValue]].
+				map(x => (x("relationship") \ "user_budget_money").as[Double]).sum
+			(Some(Map("result" -> toJson(total ++ Map("used" -> toJson(used))))), None)
+		} catch{
+			case ex: Exception => (None, Some(ErrorCode.errorToJson(ex.getMessage)))
+		}
+	}
+	
+	val formatHumans: String Map JsValue => (Option[String Map JsValue], Option[JsValue]) = { m =>
+		try {
+			val data = m("scenario")
+			val total = (data \ "current" \ "connect_reso").as[List[String Map JsValue]].
+					find(f => f("type").as[String] == "day").map(x => (x("relationship") \ "total_budget_day").as[Int]).
+					getOrElse(throw new Exception("is null"))
+			
+			val reVal = (data \ "current" \ "dest_goods_reso").as[List[String Map JsValue]].groupBy(g => g("reso_id").as[String]).flatMap { x =>
+				(data \ "current" \ "connect_reso").as[List[String Map JsValue]].find(f => f("id").as[String] == x._1).map { y =>
+					val used = x._2.map(z => (z("relationship") \ "user_budget_day").as[Double]).sum
+					Map("name" -> y("rep_name"),
+						"total" -> toJson(total),
+						"used" -> toJson(used))
+				}
+			}.toList
+			(Some(Map("result" -> toJson(reVal))), None)
+		} catch {
+			case ex: Exception => (None, Some(ErrorCode.errorToJson(ex.getMessage)))
+		}
+	}
+	
+	val formatHospitalDetails: String Map JsValue => (Option[String Map JsValue], Option[JsValue]) = { m =>
+		try {
+			val data = m("scenario")
+			
+		} catch {
+			case ex: Exception => (None, Some(ErrorCode.errorToJson(ex.getMessage)))
+		}
 	}
 }
--- app/module/scenarios/format/ScenarioFormatModule.scala
+++ app/module/scenarios/format/ScenarioFormatModule.scala
@@ -1,21 +1,19 @@
 package module.scenarios.format
 
 import play.api.libs.json.JsValue
-import play.api.libs.json.Json._
 import module.scenarios.ProposalMessage._
 import com.pharbers.bmpattern.ModuleTrait
 import com.pharbers.bmmessages.{CommonModules, MessageDefines}
 
 object ScenarioFormatModule extends ModuleTrait with ParsingTrait {
 	
-	val formatHospital: String Map JsValue => (Option[String Map JsValue], Option[JsValue]) = { m =>
-		(Some(filterZip(m("scenario"))(zfcDestGoods)), None)
-	}
-
     def dispatchMsg(msg: MessageDefines)(pr: Option[Map[String, JsValue]])
                    (implicit cm: CommonModules): (Option[Map[String, JsValue]], Option[JsValue]) = msg match {
 
-        case msg_formatQueryHospLst(data) => ps2m(pr)(formatHospital)
+        case msg_formatQueryHospLst(data) => ps2m(pr)(formatHospitals)
+        case msg_formatQueryBudget(data) => ps2m(pr)(formatBudget)
+        case msg_formatQueryHumans(data) => ps2m(pr)(formatHumans)
+        case msg_formatQueryHospitalDetails(data) => ps2m(pr)(formatHospitalDetails)
         case _ => ???
     }
 }
