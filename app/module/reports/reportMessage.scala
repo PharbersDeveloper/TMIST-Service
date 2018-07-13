a83a09ac91b1601b72e7a107e2c0b218cb1a4889
clock <2567729201@qq.com>
2018-07-13 10:33:12 +0800

    清除垃圾代码

--- app/module/reports/reportMessage.scala
+++ app/module/reports/reportMessage.scala
@@ -1,27 +1,17 @@
-package module.proposals
+package module.reports
 
 import play.api.libs.json.JsValue
 import com.pharbers.bmmessages.CommonMessage
-import module.proposals.scenarios.ScenarioModule
 
 /**
-  * Created by clock on 18-6-11.
+  * Created by clock on 18-7-13.
   */
-abstract class msg_ProposalCommand extends CommonMessage("proposal", ProposalModule)
+abstract class msg_ReportCommand extends CommonMessage("report", ReportModule)
 
 object ProposalMessage {
-    case class msg_pushProposal(data: JsValue) extends msg_ProposalCommand
-    case class msg_popProposal(data : JsValue) extends msg_ProposalCommand
-    case class msg_updateProposal(data : JsValue) extends msg_ProposalCommand
-    case class msg_queryProposal(data : JsValue) extends msg_ProposalCommand
-    case class msg_queryUsersByProposal(data : JsValue) extends msg_ProposalCommand
-    case class msg_formatProposalName(data : JsValue) extends msg_ProposalCommand
-
-
-
-    case class msg_getTotalReport(data: JsValue) extends msg_ReportCommand
-    case class msg_getHospProdReport(data : JsValue) extends msg_ReportCommand
-    case class msg_getRepProdReport(data : JsValue) extends msg_ReportCommand
-    case class msg_getResourceIO(data : JsValue) extends msg_ReportCommand
-    case class msg_getRepIndResources(data : JsValue) extends msg_ReportCommand
+    case class msg_queryTotalReport(data: JsValue) extends msg_ReportCommand
+    case class msg_queryDestsGoodsReport(data : JsValue) extends msg_ReportCommand
+    case class msg_queryResosGoodsReport(data : JsValue) extends msg_ReportCommand
+    case class msg_queryResosIO(data : JsValue) extends msg_ReportCommand
+    case class msg_queryRepIndResos(data : JsValue) extends msg_ReportCommand
 }
